ea72d11a9ce0e405b751e14e709fbf94f54b1c0c
Daniel Smanioto <daniel.smanioto@dextra-sw.com>
2020-04-17 14:38:58 -0200

    remondo codigo desnecessarios

--- database-mysql--compose-daniel/ds-mysql-connection/src/main/java/com/dsmanaioto/mysqlconnection/dsmysqlconnection/domain/User.java
+++ database-mysql--compose-daniel/ds-mysql-connection/src/main/java/com/dsmanaioto/mysqlconnection/dsmysqlconnection/domain/User.java
@@ -1,8 +1,6 @@
 package com.dsmanaioto.mysqlconnection.dsmysqlconnection.domain;
 
 import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
